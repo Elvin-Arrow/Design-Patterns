2930d7f7d6ee46e565ddf037a2bc9cb413ab6889
Muhammad Talha Bin Mansoor <m.talhabinmansoor@gmail.com>
2020-11-13 10:15:28 +0500

    Add project files.

---
+++ Factory - November 6, 2020/Examination.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory___November_6__2020
+{
+    class Examination
+    {
+    }
+}
---
+++ Factory - November 6, 2020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory___November_6__2020
+{  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
