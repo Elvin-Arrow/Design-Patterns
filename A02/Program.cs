172f28f8f82341d8b8b6014bb0c49337ca74177a
MUHAMMAD OSAMA SALEEM <33492161+IAMOSAMA111@users.noreply.github.com>
2020-11-22 18:37:49 +0500

    Update Program.cs
    c2

--- A02/Program.cs
+++ A02/Program.cs
@@ -8,81 +8,70 @@ namespace A02
 {
     public abstract class Test
     {
-        private string _Title {get;set;}
+        private string _Title { get; set; }
         public string Title => _Title;
-        public Test (string title)
+        public Test(string title)
         {
             _Title = title;
-         
         }
-
-
-
-
     }
 
-    public class Assignment : Test{
-        public Assignment (string title): base(title){}
-    
+    public class Assignment : Test
+    {
+        public Assignment(string title) : base(title) { }
+        
+    }
+    public class Quiz : Test
+    {
+        public Quiz(string title) : base(title) { }
+    }
+    public class Sessional : Test
+    {
+        public Sessional(string title) : base(title) { }
+    }
+    public class Final : Test
+    {
+        public Final(string title) : base(title) { }
+    }
+    public class Other : Test
+    {
+        public Other(string title) : base(title) { }
     }
 
-    public class Quiz : Test{
-
-         public Quiz (string title): base(title){}
-    }
-
-    public class Sessional : Test{
-
-         public Sessional (string title): base(title){}
-    
-    }
-
-    public class Final : Test{
-
-         public Final (string title): base(title){}
-
-    
-    }
-
-    public abstract class ExamFactory{
+    public abstract class Exam
+    {
+        private string _Title { get; set; }
+        public string Title => _Title;
 
         private List<Test> _Tests = new List<Test>();
+        public List<Test> Tests => _Tests;
 
-        public List <Test> Tests => _Tests;
-
-        public ExamFactory()
+        public Exam(string title)
         {
+            _Title = title;
             this.Create();
-        
         }
-
         public abstract void Create();
-    
     }
 
-    public class Exam : ExamFactory{
-
-
+    public class ExamFactory : Exam
+    {
+        public ExamFactory(string title) : base(title) { }
         public override void Create()
         {
-            Tests.Add( new Assignment("Assignment-01"));
-            Tests.Add( new Assignment("Assignment-02"));
-            Tests.Add( new Assignment("Assignment-03"));
-            Tests.Add( new Assignment("Assignment-04"));
-            Tests.Add( new Quiz("Quiz-01"));
-            Tests.Add( new Quiz("Quiz-02"));
-            Tests.Add( new Quiz("Quiz-03"));
-            Tests.Add( new Quiz("Quiz-04"));
-            Tests.Add( new Quiz("Quiz-05"));
-            Tests.Add( new Sessional("Sessional-01"));
-            Tests.Add( new Sessional("Sessional-02"));
-            Tests.Add( new Final(("Final-01"));
-
-
-
+            Tests.Add(new Assignment("Assignment - 1"));
+            Tests.Add(new Assignment("Assignment - 2"));
+            Tests.Add(new Assignment("Assignment - 3"));
+            Tests.Add(new Assignment("Assignment - 4"));
+            Tests.Add(new Quiz("Quiz - 1"));
+            Tests.Add(new Quiz("Quiz - 2"));
+            Tests.Add(new Quiz("Quiz - 3"));
+            Tests.Add(new Quiz("Quiz - 4"));
+            Tests.Add(new Sessional("Sessional - 1"));
+            Tests.Add(new Sessional("Sessional - 2"));
+            Tests.Add(new Final("Fianl Term"));
+            Tests.Add(new Other("Other type"));
         }
-
-
     }
 
 
@@ -97,17 +86,19 @@ namespace A02
             Console.WriteLine("Assignment No- 02");
             List <ExamFactory> exams = new List<ExamFactory>()
             {
-                new Exam(),  // Fall 2020
-                new Exam(),  // spring 2020
+                new ExamFactory("Fall 2020"),
+                new ExamFactory("Spring 2021"),
+                new ExamFactory("Fall 2021"),
+                new ExamFactory("Spring 2022"),
             
             };
             var index =1;
             foreach (var exam in exams)
             {
-                Console.WriteLine("\n"+ (index++)+ ".  "+exam.GetType().Name);
-                foreach(var test in exam.Tests){
-                    Console.WriteLine("   "+test.GetType().Name);
-                
+                Console.WriteLine("\n" + $"{index++}.  {exam.Title}  -  {exam.GetType().Name}");
+                foreach (var test in exam.Tests)
+                {
+                    Console.WriteLine("".PadRight(10) + $".  {test.Title}  -  {test.GetType().Name}");
                 }
             }
 
