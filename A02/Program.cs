37e55fb4e45f642213970e3f9ed0072e8a67056e
MUHAMMAD OSAMA SALEEM <33492161+IAMOSAMA111@users.noreply.github.com>
2020-11-22 22:10:33 +0500

    Update Program.cs
    c2

--- A02/Program.cs
+++ A02/Program.cs
@@ -6,34 +6,46 @@ using System.Threading.Tasks;
 
 namespace A02
 {
-    public abstract class Test
+    public abstract class Info
     {
         private string _Title { get; set; }
         public string Title => _Title;
-        public Test(string title)
+        public Info(string title)
         {
             _Title = title;
         }
     }
 
-    public class Assignment : Test
+    public class Student : Info{
+        public Student(string title) : base(title) {
+        }
+    
+    }
+
+    public class Group : Info{
+        public Group(string title) : base(title) {
+        }
+    }
+
+
+    public class Assignment : Info
     {
         public Assignment(string title) : base(title) { }
         
     }
-    public class Quiz : Test
+    public class Quiz : Info
     {
         public Quiz(string title) : base(title) { }
     }
-    public class Sessional : Test
+    public class Sessional : Info
     {
         public Sessional(string title) : base(title) { }
     }
-    public class Final : Test
+    public class Final : Info
     {
         public Final(string title) : base(title) { }
     }
-    public class Other : Test
+    public class Other : Info
     {
         public Other(string title) : base(title) { }
     }
@@ -43,8 +55,8 @@ namespace A02
         private string _Title { get; set; }
         public string Title => _Title;
 
-        private List<Test> _Tests = new List<Test>();
-        public List<Test> Tests => _Tests;
+        private List<Info> _Tests = new List<Info>();
+        public List<Info> Tests => _Tests;
 
         public Exam(string title)
         {
@@ -59,6 +71,8 @@ namespace A02
         public ExamFactory(string title) : base(title) { }
         public override void Create()
         {
+            Tests.Add(new Student("Muhammad Talha Bin Mansoor"));
+            Tests.Add(new Group("Group-1"));
             Tests.Add(new Assignment("Assignment - 1"));
             Tests.Add(new Assignment("Assignment - 2"));
             Tests.Add(new Assignment("Assignment - 3"));
@@ -79,11 +93,98 @@ namespace A02
 
 
 
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Assignment No- 02");
+
+            
+
+            int atr = 1;
+
+            while (atr != 0) {
+                Console.WriteLine(" Enter 1 to add student \n Enter 2 to add data of student \n Enter 3 for see all students \n Enter 4 to exit");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Data Entry");
+
+                        Console.WriteLine("Enter Student ID");
+                        string stdID = Console.ReadLine();
+                        Console.WriteLine("Do you want to enter assignment Marks... (y/n)");
+                        string check = Console.ReadLine();
+                        if(check == "y")
+                        {
+                            Console.WriteLine("Enter title");
+                            string asign = Console.ReadLine();
+                            Console.WriteLine("Enter Obtained marks");
+                            string asignObtn = Console.ReadLine();
+                            Console.WriteLine("Enter Total  marks");
+                            string asignTot = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("Do you want to enter Quiz Marks... (y/n)");
+                        string qcheck = Console.ReadLine();
+                        if (qcheck == "y")
+                        {
+                            Console.WriteLine("Enter title");
+                            string asign = Console.ReadLine();
+                            Console.WriteLine("Enter Obtained marks");
+                            string asignObtn = Console.ReadLine();
+                            Console.WriteLine("Enter Total  marks");
+                            string asignTot = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("Do you want to enter Sessional Marks... (y/n)");
+                        string scheck = Console.ReadLine();
+                        if (scheck == "y")
+                        {
+                            Console.WriteLine("Enter title");
+                            string asign = Console.ReadLine();
+                            Console.WriteLine("Enter Obtained marks");
+                            string asignObtn = Console.ReadLine();
+                            Console.WriteLine("Enter Total  marks");
+                            string asignTot = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("Do you want to enter final Marks... (y/n)");
+                        string fcheck = Console.ReadLine();
+                        if (fcheck == "y")
+                        {
+                            Console.WriteLine("Enter title");
+                            string asign = Console.ReadLine();
+                            Console.WriteLine("Enter Obtained marks");
+                            string asignObtn = Console.ReadLine();
+                            Console.WriteLine("Enter Total  marks");
+                            string asignTot = Console.ReadLine();
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter Student Id");
+                        break;
+
+                    case 3:
+                        break;
+
+                    default:
+
+                        break;
+
+
+                }
+
+
+
+            }
+            
+
+
             List <ExamFactory> exams = new List<ExamFactory>()
             {
                 new ExamFactory("Fall 2020"),
